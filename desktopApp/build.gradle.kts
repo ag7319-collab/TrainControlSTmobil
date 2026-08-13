dd285026ac215a2be29194aad35cfbe619359d9c
ag7319-collab <ag7319@gmail.com>
2026-08-13 21:22:39 +0200

    Update version to 1.0.6 graphical fixes

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -36,7 +36,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "TrainControlSTmobil"
-            packageVersion = "1.0.5"
+            packageVersion = "1.0.6"
             copyright = "© 2026 g.andi"
             description = "Zug-Anzeige Südtirol"
         }
